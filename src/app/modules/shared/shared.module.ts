bac48cd614c886d401276a8ed50cb0ff3406d7ff
Nikita <nikita.babych@wearehathway.com>
2020-09-08 15:45:38 +0300

    Merged context menu module and shared module.

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { GetPipe } from './pipes/get.pipe';
+import { contextMenuReducers } from './reducers';
+import { ContextMenuEffects } from './effects/context-menu.effects';
+import { ContextMenuContainer } from './containers/context-menu-container/context-menu-container.component';
+import { ContextMenuInitializeDirective } from './directives/context-menu-initialize.directive';
 
 @NgModule({
   declarations: [
-    GetPipe
+    GetPipe,
+    ContextMenuContainer,
+    ContextMenuInitializeDirective,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('contextMenu', contextMenuReducers),
+    EffectsModule.forFeature([ContextMenuEffects]),
   ],
   exports: [
     GetPipe,
+    ContextMenuContainer,
+    ContextMenuInitializeDirective,
   ]
 })
 export class SharedModule { }
