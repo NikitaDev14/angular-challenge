60450abd876ed3d6f0af897e1365304c6b464ccd
Nikita <nikita.babych@wearehathway.com>
2021-02-15 22:38:44 +0200

    [Optimization] Pipe Filter. [Refactor] Subscribe form decomposition. Ticker input custom form control. [Fix] Immutability checks.

---
+++ src/app/modules/stock/components/ticker-input/ticker-input.component.ts
@@ -0,0 +1,70 @@
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import { StockSymbol } from '../../models/symbol.models';
+
+@Component({
+  selector: 'app-ticker-input',
+  templateUrl: './ticker-input.component.html',
+  styleUrls: ['./ticker-input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => TickerInputComponent),
+    multi: true,
+  }],
+})
+export class TickerInputComponent implements OnInit, ControlValueAccessor {
+  @Input() symbols: StockSymbol[];
+
+  focusSubject$ = new Subject<boolean>();
+  inputSubject$ = new Subject<string>();
+
+  input$: Observable<string>;
+  showAutocomplete$: Observable<boolean>;
+
+  inputValue: string;
+
+  ngOnInit(): void {
+    this.input$ = this.inputSubject$.asObservable().pipe(
+      shareReplay(1),
+    );
+
+    this.showAutocomplete$ = combineLatest([
+      this.focusSubject$.asObservable(),
+      this.input$,
+    ]).pipe(
+      map(([isFocus, inputValue]: [boolean, string]) =>
+        isFocus && inputValue.length > 0,
+      ),
+    );
+  }
+
+  onFocusOut($event: FocusEvent) {
+    $event.relatedTarget === null && this.focusSubject$.next(false);
+  }
+
+  onSelectTicker(stockSymbol: StockSymbol) {
+    if (!stockSymbol) {
+      return;
+    }
+
+    this.onChange(stockSymbol.displaySymbol);
+    this.focusSubject$.next(false);
+  }
+
+  onChange = (value: string) => {};
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  writeValue(value: string): void {
+    this.inputSubject$.next(value);
+  }
+
+}
